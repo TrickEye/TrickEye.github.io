57dd327ec63add751424de7430aaf7498e4e48b8
TrickEye <85970705+TrickEye@users.noreply.github.com>
2023-09-19 08:30:30 +0800

    router update

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,14 @@ import './style.css'
 import App from './App.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-import Home from './pages/Home.vue'
-import Blogs from './pages/Blogs.vue'
-import B0 from './blogs/B0/B0.vue'
-import B1 from './blogs/B1/B1.vue'
-import B2 from './blogs/B2/B2.vue'
-import About from './pages/About.vue'
-
 const routes = [
-    { path: '/', component: Home },
-    { path: '/blogs', component: Blogs },
-    { path: '/blogs/B0', component: B0 },
-    { path: '/blogs/B1', component: B1 },
-    { path: '/blogs/B2', component: B2 },
-    { path: '/about', component: About },
+    { path: '/', component: () => import('./pages/Home.vue') },
+    { path: '/blogs', component: () => import('./pages/Blogs.vue') },
+    { path: '/blogs/B0', component: () => import('./blogs/B0/B0.vue') },
+    { path: '/blogs/B1', component: () => import('./blogs/B1/B1.vue') },
+    { path: '/blogs/B2', component: () => import('./blogs/B2/B2.vue') },
+    { path: '/blogs/B3', component: () => import('./blogs/B3/B3.vue') },
+    { path: '/about', component: () => import('./pages/About.vue') },
 ]
 
 const router = createRouter({
